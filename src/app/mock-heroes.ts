52c1e80b465426359e17761a22c21f7ffe87902a
elKryptos <hans_09@hotmail.it>
2024-11-25 17:45:53 +0100

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero"
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Dr. Nice'},
+    { id: 2, name: 'Bombasto'},
+    { id: 3, name: 'Celeritas'},
+    { id: 4, name: 'Magneta'},
+    { id: 5, name: 'RubberMan'},
+    { id: 6, name: 'Diname'},
+    { id: 7, name: 'Dr. IQ'},
+    { id: 8, name: 'Magma'},
+    { id: 9, name: 'Biden'},
+    { id: 10, name: 'Trump'}
+]
