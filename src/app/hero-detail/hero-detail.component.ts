f020a8e2cea1fd059d0984ef419df8cccb6a1811
elKryptos <hans_09@hotmail.it>
2024-11-26 10:22:16 +0100

    added new component

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styles: [
+  ]
+})
+export class HeroDetailComponent {
+
+  @Input() hero?: Hero
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { NgFor } from '@angular/common';
-
 
 @Component({
   selector: 'app-heroes',
@@ -12,12 +10,12 @@ import { NgFor } from '@angular/common';
 
 export class HeroesComponent {
 
-  selectedHero!: Hero
+  selectedHero?: Hero
   heroes = HEROES
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };  
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };  
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
